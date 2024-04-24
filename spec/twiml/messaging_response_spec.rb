312ba1a4381f7c1e5ded58d0d0159422fbb80126
Soumyajit Das <soumyajit.das@harness.io>
2024-04-24 20:14:40 +0530

    Update messaging_response_spec.rb

--- spec/twiml/messaging_response_spec.rb
+++ spec/twiml/messaging_response_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-# weksdajmkkkjjmnkjnjkbjb
+# weksdajmkkkjjmnkjnjkbjbad
 describe Twilio::TwiML::MessagingResponse do
   context 'Testing Response' do
     it 'should allow empty response' do
