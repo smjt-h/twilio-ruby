17dc88375e784b9f01a0bceebc5fd7a2b4782ebd
Soumyajit Das <soumyajit.das@harness.io>
2024-05-16 22:28:57 +0530

    Update configuration_spec.rb

--- spec/util/configuration_spec.rb
+++ spec/util/configuration_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-# dgsgsdgadajgqwffk
+# dgsgsdgadajgqwffkads
 describe Twilio::Util::Configuration do
   it 'should have an account sid attribute' do
     config = Twilio::Util::Configuration.new
